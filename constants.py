12d44c7b8b04536172383bce16bf0e644f47bd23
Carlos Valdes <valdescarlos17@gmail.com>
2024-03-15 13:23:19 -0300

    url base

--- constants.py
+++ constants.py
@@ -1,4 +1,8 @@
+import os
+heroku_app_name = os.getenv('HEROKU_APP_NAME')
+
+
 CURRENCY_CLP = 'CLP'
 FRONT_URL = 'http://localhost:5173'
 FINISH_URL = '/checkout/finish'
-API_URL = 'http://127.0.0.1:8000'
+API_URL = f'https://{heroku_app_name}.herokuapp.com' if heroku_app_name else 'http://127.0.0.1:8000'
