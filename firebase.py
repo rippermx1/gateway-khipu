9831d7f25bd27b14af2f656447042cc155b8b2fb
Carlos Valdes <valdescarlos17@gmail.com>
2024-03-15 13:03:23 -0300

    firebase

--- firebase.py
+++ firebase.py
@@ -5,13 +5,14 @@ import uuid
 import os
 import json
 
+path = "./default.json"
 firebase_credentials = os.getenv('FIREBASE_CREDENTIALS')
+print(firebase_credentials)
+
 if firebase_credentials:
     with open("default.json", "w") as write_file:
             json.dump(firebase_credentials, write_file)
-else:
-    firebase_credentials = "default.json"
-app = firebase_admin.initialize_app(credentials.Certificate(firebase_credentials))
+app = firebase_admin.initialize_app(credentials.Certificate(path))
 
 
 class FirebaseDatabase:
